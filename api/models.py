d9c3721d23d6c6f29fa24b9810d69f8622499792
Adi <adiboroica235@gmail.com>
2022-06-09 10:11:21 +0100

    Add fields to the Event Model

--- api/models.py
+++ api/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
+
 class Event(models.Model):
-  name = models.CharField(max_length=50)
-  date = models.DateField()
-  start_time = models.TimeField()
-  end_time = models.TimeField()
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=100)
+    details = models.CharField(max_length=200)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    price = 40
